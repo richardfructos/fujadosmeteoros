cb0426a104dc391afbcf476363567e4bb6b51ffb
richardfructos <46432000+richardfructos@users.noreply.github.com>
2019-01-07 03:59:26 -0200

    Add files via upload

---
+++ HTML5/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1546838434,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg.png",
+		"images/img_fundo-sheet0.png",
+		"images/leg_titulo-sheet0.png",
+		"images/bt_play-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/player-sheet0.png",
+		"images/enemydestroy-sheet0.png",
+		"images/btn_home-sheet0.png",
+		"images/btn_restart-sheet0.png",
+		"images/pf-sheet0.png",
+		"media/buttons (1).ogg",
+		"media/morte.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
